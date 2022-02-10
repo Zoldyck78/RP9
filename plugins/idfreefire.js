77a9c13b5845a31ccdbe807313e04d05fe4c0a5c
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-10 08:30:25 +0700

    p

--- plugins/idfreefire.js
+++ plugins/idfreefire.js
@@ -1,10 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. id nya mana?\n\ncontoh:\n${usedPrefix + command} 1906651269`
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/id/epep?apikey=${bearkey}&id=${args[0]}`)
-    if (!res.ok) throw eror
+  let res = await fetch(global.API('xteam', '/search/freefire', { id: args[0] }, 'APIKEY'))
+  if (!res.ok) throw eror
   let json = await res.json()
-  m.reply(json.result)
+  if (!json.status) throw json
+  m.reply(json.result.name)
 }
 handler.help = ['epep'].map(v => v + ' <id>')
 handler.tags = ['internet']
