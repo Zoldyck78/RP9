589f1e296827a487a87a8c53df892e1b3c8a43fc
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-12 07:55:06 +0700

    p

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       await conn.loadAuthInfo(obj)
       auth = true
     }
-    conn.on('qr', async qr => {
+    conn.on('qr', function(qr) => {
       let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk Perangkat Tertaut\n3. Scan QR ini \nQR Expired dalam 20 detik', m)
       setTimeout(() => {
         parent.deleteMessage(m.chat, scan.key)
