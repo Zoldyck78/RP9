229cf1cfef1ad74f41fa324ada79c71d08fa5e66
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-15 09:21:00 +0700

    p

--- plugins/menu.js
+++ plugins/menu.js
@@ -196,10 +196,9 @@ let handler = async (m, { conn, usedPrefix: _p, command }) => {
            },
                {
              quickReplyButton: {
-               displayText: 'YukiBot Group',
-               id: 'https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL',
+               displayText: 'YukiBot 𝚝𝚎𝚊𝚖',
+               id: '.tqto',
              }
-
            },
                {
              quickReplyButton: {
