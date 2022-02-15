06106aa264a34407ea3fe3a118a8fc934a63ef2d
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-15 09:33:54 +0700

    p

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,12 +1,50 @@
-let handler = async (m, { conn }) => {
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
+let moment = require('moment-timezone')
+let fs = require('fs')
+let handler = async (m, { conn, command }) => {
+let img1 = fs.readFileSync('./src/img1.png')
+let img2 = fs.readFileSync('./src/img2.png')
+let user = global.DATABASE.data.users[m.sender]
+/*conn.sendFile(m.chat, img, '', `Halo juga kak ${conn.getName(m.sender)}`, { key: { fromMe: false, remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { message: `${ucapan()} ${conn.getName(m.sender)}`, itemCount: 999, thumbnail: fs.readFileSync('./src/mikey.jpg')
+}}})*/
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = m.fromMe ? conn.user : conn.contacts[who]
+pushname2 = `*${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])}*`
 
-m.reply(`
-┌─〔 R U N T I M E 〕
-├ Bot Aktif Selama ${uptime}
-└────
-    `.trim())
+function kyun(seconds){
+function pad(s){
+return (s < 10 ? '0' : '') + s;
+}
+var hours = Math.floor(seconds / (60*60));
+var minutes = Math.floor(seconds % (60*60) / 60);
+var seconds = Math.floor(seconds % 60);
+
+//return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+return `${pad(hours)}Jam ${pad(minutes)}Menit ${pad(seconds)}Detik`
+}
+runtime = process.uptime()
+					teks = `𝑲𝒊𝒏𝒈 𝑶𝒇 𝑩𝒆𝒂𝒓 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍`// \n\n${kyun(runtime)}`
+					run = `${kyun(runtime)}`
+					var itsme = `0@s.whatsapp.net`
+					var split = `teks`
+					const ftrol = {
+	key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 999,
+                            status: 1,
+                            surface : 1,
+                            message: `${teks}\n${run}`, //Kasih namalu
+                            orderTitle: `${teks}\n${run}`,
+                            thumbnail: img1, //Gambarnye
+                            sellerJid: '0@s.whatsapp.net' 
+                          }
+                        }
+                      }
+					//m.reply(teks)
+					
+conn.sendMessage(m.chat, `*Bot aktif selama*\n${run}\n\n_©King Of Bear_`, 'conversation', {quoted: ftrol})
 }
 handler.help = ['runtime']
 handler.tags = ['info']
