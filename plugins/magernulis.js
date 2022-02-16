1ba8db25ce68c9942c70e1a270231ad61c6f53ce
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-02-16 10:21:40 +0700

    p

--- plugins/magernulis.js
+++ plugins/magernulis.js
@@ -6,7 +6,7 @@ let handler = async (m, { command, conn, args, usedPrefix }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.status != true) throw json
-conn.sendFile(m.chat, json.result, 'eror.jpg', `Ni kak udah jadi hehe:v\n${footer}`, m, 0, { thumbnail: await (await fetch(json.result)).buffer() })
+conn.sendFile(m.chat, json.result, 'eror.jpg', `Ni kak udah jadi hehe:v\n\n_${global.wm}_`, m, 0, { thumbnail: await (await fetch(json.result)).buffer() })
 }
 handler.help = new Array(6).fill('magernulis').map((v, i) => v + (i + 1) + ' <teks>')
 handler.tags = ['nulis']
